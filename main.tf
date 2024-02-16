49870ff178a0a8ff9f0e352580a93805b051c2e3
vedams11 <91414735+vedams11@users.noreply.github.com>
2024-02-16 17:40:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.37.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
 data "aws_caller_identity" "current" {}
 
 output "account_id" {
